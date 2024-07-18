d5458b900da604dde04fbcf7d1f21b4e3a64e1c1
Jakub Łosiowski <jlosiowski@protonmail.com>
2024-07-18 11:44:18 +0200

    init

---
+++ app/src/main/java/aoc/day01/Day01.java
@@ -0,0 +1,19 @@
+package aoc.day01;
+
+import aoc.Day;
+
+import java.util.List;
+
+public class Day01 implements Day {
+
+    @Override
+    public String part1(List<String> input) {
+        return input.isEmpty() ? "" : input.get(0);
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        return input.isEmpty() ? "" : input.get(0);
+    }
+
+}
